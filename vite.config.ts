f613ff76ba702d6788fd5679f8b95f1aa8bf80b1
madissonrt <madissonrt@gmail.com>
2026-04-03 14:40:36 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/floreria-amelietprueba/',
   plugins: [react()],
   server: {
     host: true, // Esto permite el acceso desde la red
